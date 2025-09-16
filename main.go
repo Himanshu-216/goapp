065c27ffd9ccac81f2e7db40fa46c9d57c9c3176
Himanshu <himanshu162pnt@gmail.com>
2025-09-16 20:54:22 +0530

    test k9

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 func handler(w http.ResponseWriter, r *http.Request) {
 	serviceName := os.Getenv("SERVICE_NAME") // read from env variable
 	if serviceName == "" {
-		serviceName = "Flux"
+		serviceName = "Kubernetes Cluster"
 	}
 	fmt.Fprintf(w, "Hello and Welcome from %s!", serviceName)
 }
